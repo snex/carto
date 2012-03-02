e3375cf3692dd6cef1db447a8be29f7a46603472
Dave Havlicek <xens@comcast.net>
2012-03-02 17:36:46 -0600

    fix command line parsing

--- carto.rb
+++ carto.rb
@@ -5,17 +5,17 @@ require 'lib/carto'
 
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: ruby carto.rb -m map_file -o output_file -t tileset"
+  opts.banner = "Usage: ruby carto.rb -m map-file -o output_file -t tileset"
 
-  opts.on("-m", "--map_file", "Map File") do |m|
+  opts.on("-m", "--map-file [MAP_FILE]", "TSV Map File") do |m|
     options[:map_file] = m
   end
 
-  opts.on("-o", "--output_file", "Output file") do |o|
+  opts.on("-o", "--output-file [OUTPUT_FILE]", "Output rpmap file") do |o|
     options[:output_file] = o
   end
 
-  opts.on("-t", "--tileset", "Tileset") do |t|
+  opts.on("-t", "--tileset [TILESET]", "Tileset") do |t|
     options[:tileset] = t
   end
 end.parse!
