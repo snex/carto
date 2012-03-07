c120d7d1f000347c05713b627dc82ca84584625a
Dave Havlicek <xens@comcast.net>
2012-03-07 00:15:28 -0600

    make map-file required argument

--- carto.rb
+++ carto.rb
@@ -4,7 +4,7 @@ $: << 'lib/'
 require 'lib/carto'
 
 options = {}
-OptionParser.new do |opts|
+optparse = OptionParser.new do |opts|
   opts.banner = "Usage: ruby carto.rb -m map-file -o output_file -t tileset"
 
   opts.on("-m", "--map-file [MAP_FILE]", "TSV Map File") do |m|
@@ -18,7 +18,22 @@ OptionParser.new do |opts|
   opts.on("-t", "--tileset [TILESET]", "Tileset") do |t|
     options[:tileset] = t
   end
-end.parse!
+end
+
+begin
+  optparse.parse!
+  mandatory = [:map_file]
+  missing = mandatory.select { |param| options[param].nil? }
+  if !missing.empty?
+    puts "Missing Options: #{missing.join(', ')}"
+    puts optparse
+    exit
+  end
+rescue OptionParser::InvalidOption, OptionParser::MissingArgument
+  puts $!.to_s
+  puts optparse
+  exit
+end
 
 carto = Carto.new options
 carto.write_map
